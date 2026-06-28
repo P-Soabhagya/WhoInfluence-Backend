bfe2bcfdefbb6143b8ce5c5ba83b022c8d2aff7d
Vareen Aggarwal <vareennimi@gmail.com>
2026-06-29 01:16:26 +0530

    videos done

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,7 +29,16 @@ const influencerRouter = require('./routes/influencer');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:3000'] }));
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'http://localhost:5175',
+    'http://localhost:3000',
+    'https://who-influence.netlify.app'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 app.use('/videos', express.static(path.join(__dirname, '../videos')));
